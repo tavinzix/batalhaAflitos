44f510af120fd80c90cf9e8602501c771e59dfc2
Otávio Fagundes <otaviofariafagundes1@gmail.com>
2024-09-01 23:25:12 -0300

    organização das fotos

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById('menu-toggle');
-    const navList = document.getElementById('nav-list');
-
-    menuToggle.addEventListener('click', function () {
-        navList.classList.toggle('active');
-    });
-});
